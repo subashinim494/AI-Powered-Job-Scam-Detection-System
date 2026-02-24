1e5f04260566c7ecae820a6e9e5c41f92a8cf980
subashinim494 <subashinim494@gmail.com>
2026-02-24 15:15:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -167,6 +167,7 @@ def predict():
     )
 
 # -------------------- RUN APP --------------------
+import os
+
 if __name__ == "__main__":
-    if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
